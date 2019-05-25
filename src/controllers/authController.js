91f926b740bd0db8bdf6dc4c076b630943960a3f
Marcos Frony <maruchan@Marcoss-MacBook-Pro.local>
2019-05-24 18:33:00 -0700

    Add routesController

---
+++ src/controllers/authController.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const { MongoClient, ObjectID } = require('mongodb');
+const Promise = require('bluebird');
+const bcrypt = Promise.promisifyAll(require("bcrypt"));
+const debug = require('debug')('app:authController');
+const config = require('../config/db.json');
+const { registrationSchema } = require('../helpers/validationSchema');
+// const AUDIO_DIR = '/audio/';
+
+function authController(nav) {
+  function registerUser(req, res) {
+    debug('Signing up!');
+    const {
+      firstname,
+      lastname,
+      username,
+      password,
+    } = req.body;
+
+    // validate form
+    req.checkBody(registrationSchema);
+    const errors = req.validationErrors();
+    debug(errors);
+
+    if (errors) {
+      const arrAcc = [];
+      const prunedErrors = errors.map(err => {
+        const param = err.param;
+        if (!arrAcc.includes(param)) {
+          arrAcc.push(param);
+          return err;
+        }
+      }).filter(err => typeof err !== "undefined");
+      debug(prunedErrors);
+      const errorObj = {
+        nav,
+        title: 'Jukebox: Sign Up',
+        errors: prunedErrors,
+      };
+      return res.render('signup', errorObj);
+    }
+
+    // set up mongo stuff
+    const url = config.dbURL;
+    const dbName = config.dbName;
+    const colName = config.collectionUsersName;
+
+    (async function addUser(){
+      let client;
+      try {
+        client = await MongoClient.connect(url, { useNewUrlParser: true } );
+        debug('Connected to mongo server');
+
+        // Use jukebox database
+        const db = client.db(dbName);
+
+        // Use users collection
+        const col = db.collection(colName);
+
+        // prevent existing email from signing up again
+        const existingUser = await col.findOne({ username });
+        if (existingUser) {
+          const dataObj = {
+            nav,
+            title: 'Jukebox: Sign Up',
+            errorMessage: 'Sorry, we could not complete your sign up',
+          };
+          return res.render('signup', dataObj);
+        }
+
+        // encrypt password
+        const hash = await bcrypt.hashAsync(password, 12);
+
+        // set up user object to be inserted
+        const user = {
+          firstname,
+          lastname,
+          username,
+          email: username,
+          password: hash,
+          created: Date.now(),
+        };
+
+        // insert user into DB
+        const results = await col.insertOne(user);
+        debug(results);
+        req.login(results.ops[0], () => {
+          debug('I am signed up after login!');
+          res.redirect('/jukebox');
+        });
+      } catch(err) {
+        if (err) {
+          debug(err.stack);
+        }
+      }
+    }());
+  }
+
+  return {
+    registerUser,
+  };
+}
+
+module.exports = authController;
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,15 +2,11 @@
 
 const express = require('express');
 const authRouter = express.Router();
-const { MongoClient } = require('mongodb');
 const passport = require('passport');
-const Promise = require('bluebird');
-const bcrypt = Promise.promisifyAll(require("bcrypt"));
-const debug = require('debug')('app:authRoutes');
-const config = require('../config/db.json');
-const { registrationSchema } = require('../helpers/validationSchema');
+const authController = require('../controllers/authController');
 
 function router(nav) {
+  const { registerUser } = authController(nav);
   authRouter.route('/signup')
     .get((req, res) => {
       res.render(
@@ -21,93 +17,7 @@ function router(nav) {
         }
       );
     })
-    .post((req, res) => {
-      debug('Signing up!');
-      const {
-        firstname,
-        lastname,
-        username,
-        password,
-      } = req.body;
-
-      // validate form
-      req.checkBody(registrationSchema);
-      const errors = req.validationErrors();
-      debug(errors);
-
-      if (errors) {
-        const arrAcc = [];
-        const prunedErrors = errors.map(err => {
-          const param = err.param;
-          if (!arrAcc.includes(param)) {
-            arrAcc.push(param);
-            return err;
-          }
-        }).filter(err => typeof err !== "undefined");
-        debug(prunedErrors);
-        const errorObj = {
-          nav,
-          title: 'Jukebox: Sign Up',
-          errors: prunedErrors,
-        };
-        return res.render('signup', errorObj);
-      }
-
-      // set up mongo stuff
-      const url = config.dbURL;
-      const dbName = config.dbName;
-      const colName = config.collectionUsersName;
-
-      (async function addUser(){
-        let client;
-        try {
-          client = await MongoClient.connect(url, { useNewUrlParser: true } );
-          debug('Connected to mongo server');
-
-          // Use jukebox database
-          const db = client.db(dbName);
-
-          // Use users collection
-          const col = db.collection(colName);
-
-          // prevent existing email from signing up again
-          const existingUser = await col.findOne({ username });
-          if (existingUser) {
-            const dataObj = {
-              nav,
-              title: 'Jukebox: Sign Up',
-              errorMessage: 'Sorry, we could not complete your sign up',
-            };
-            return res.render('signup', dataObj);
-          }
-
-          // encrypt password
-          const hash = await bcrypt.hashAsync(password, 12);
-
-          // set up user object to be inserted
-          const user = {
-            firstname,
-            lastname,
-            username,
-            email: username,
-            password: hash,
-            created: Date.now(),
-          };
-
-          // insert user into DB
-          const results = await col.insertOne(user);
-          debug(results);
-          req.login(results.ops[0], () => {
-            debug('I am signed up after login!');
-            res.redirect('/jukebox');
-          });
-        } catch(err) {
-          if (err) {
-            debug(err.stack);
-          }
-        }
-      }());
-    });
+    .post(registerUser);
 
   authRouter.route('/signin')
     .post(passport.authenticate('local', {
